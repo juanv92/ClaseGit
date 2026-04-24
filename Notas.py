9a1f5e5a599eda27579ae32fd49bc48f48463656
Juan Vanegas <juanvanvarg7@gmail.com>
2026-04-24 08:41:03 -0500

    Cree el archivo de notas

---
+++ Notas.py
@@ -0,0 +1,8 @@
+estudiantes =[
+    ['nombre':'Juan','notas':[3,0,4,0,3,0]],
+    ['nombre':'Ana','notas':[5,0,4,0,4,8]],
+    ['nombre':'Andres','notas':[3,5,3,5,4,8]]
+]
+
+print("---BASE DE DATOS CARGADA---")
+print(f"Total de estudiantes: {len(estudiantes)}")
